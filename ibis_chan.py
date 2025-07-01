aee237f30b2ce2d9fa5f9fc11bf1b7a4845bebda
Kamal Sant <203077896+santkamal@users.noreply.github.com>
2025-07-01 11:56:01 +0200

    added changes including the parallelisation part

--- ibis_chan.py
+++ ibis_chan.py
@@ -1,8 +1,11 @@
 import numpy as np
 from parmin_ibis import parmin_ibis
 from lpff_pol import lpff_pol
-from scipy.interpolate import interp1d
+from scipy.interpolate import interp1d, make_interp_spline
+from multiprocessing import Pool
+from functools import partial
 
+"""
 def ibis_chan(aps1,flat1,wscale1,wscale_equi,Npol,Nwave,Nx,Ny,npoints):
 
 
@@ -70,6 +73,75 @@ def ibis_chan(aps1,flat1,wscale1,wscale_equi,Npol,Nwave,Nx,Ny,npoints):
 
 
     return bshift_cog1,bshift_poly1
+"""
 
+def avgoffset(k, aps1, flat1, wscale1, wscale_equi, npoints, idxx, Nwave):
+    # just for a check, not used in the final calculation
+    id = np.where(aps1 >= 1, True, False)
+    avgprof = np.mean(flat1[k, :, :, :], axis=(1, 2), where=id, dtype=np.float64)
+    pavg = (make_interp_spline(wscale1, avgprof, k=2))(wscale_equi)
+    print("avgprof", avgprof)
+    print("pavg", pavg)
+    avgoffs, avgoffs1 = lpff_pol(pavg), parmin_ibis(pavg, npoints)  # Filtro passa-basso
+    print("Done", k)
+    return [avgoffs, avgoffs1]
+
+def avgoffsets(k, aps1, flat1, wscale1, wscale_equi, npoints):
+    # This is the function being used in the final calculation
+    id = np.where(aps1 >= 1, True, False)
+    avgprof = np.mean(flat1[k, :, :, :], axis=(1, 2), where=id, dtype=float)
+    pavg = (make_interp_spline(wscale1, avgprof, k=2))(wscale_equi)
+    avgoffs, avgoffs1 = lpff_pol(pavg), parmin_ibis(pavg, npoints)  # Filtro passa-basso
+    return [avgoffs, avgoffs1]
+
+def interpolate(k, aps1, flat1, wscale1, wscale_equi, npoints, Nx, Ny, idxx, idxy):
+    avgoffs, avgoffs1 = avgoffsets(k, aps1, flat1, wscale1, wscale_equi, npoints)
+    bshift_cog1, bshift_poly1 = np.zeros((Ny, Nx//2), dtype=np.float64), np.zeros((Ny, Nx//2), dtype=np.float64)
+    for i in range(idxx.size):
+        x, y = idxx[i], idxy[i]
+        p = (make_interp_spline(wscale1, flat1[k, :, y, x], k=2))(wscale_equi)
+        bshift_cog1[y, x], bshift_poly1[y, x] = (lpff_pol(p) - avgoffs), (parmin_ibis(p, npoints) - avgoffs1)
+    print("Done", k)
+    return [bshift_cog1, bshift_poly1]
     
-  
+def ibis_chan(aps1, flat1, wscale1, wscale_equi, Npol, Nwave, Nx, Ny, npoints):
+
+    # Ottieni gli indici delle aperture per i canali sinistro e destro
+    idx = np.column_stack(np.where(aps1 >= 1))
+    idxy, idxx = idx[:, 0], idx[:, 1]
+# %%
+    """
+    # This part is not strictly necessary in the running code; it is included here for verification purposes.
+    part = partial(avgoffset, aps1=aps1, flat1=flat1, wscale1=wscale1,
+                   wscale_equi=wscale_equi, npoints=npoints, idxx=idxx, Nwave=Nwave)
+    with Pool(6) as pool:
+        result = pool.map(part, ([0, 1, 2, 3, 4, 5]))
+
+    result = np.array(result)
+    print(result.shape)
+    avgoffs, avgoffs1 = result[:, 0], result[:, 1]
+    print("avgoffs, avgoffs1 = ", avgoffs, avgoffs1)
+    """
+# %%
+    
+    # Interpolate and compute polynomial fitting
+    part_interp = partial(interpolate, aps1=aps1, flat1=flat1, wscale1=wscale1,
+                    wscale_equi=wscale_equi, npoints=npoints, Nx=Nx, Ny=Ny, idxx=idxx, idxy=idxy)
+    # partial definition of the interpolation function.
+    with Pool(6) as pool:
+        result = pool.map(part_interp, ([0, 1, 2, 3, 4, 5]))
+    # Calculations for all polarisation states are done in parallel.
+
+    result = np.array(result)
+
+    bshift_cog1, bshift_poly1 = result[:, 0, :, :], result[:, 1, :, :]
+
+    if Npol == 1:
+        print("1")
+        bshift_cog1, bshift_poly1 = bshift_cog1, bshift_poly1
+
+    else:
+        print("2")
+        bshift_cog1, bshift_poly1 = np.mean(bshift_cog1, axis=0), np.mean(bshift_poly1, axis=0)
+
+    return bshift_cog1, bshift_poly1
